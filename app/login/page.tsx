3094f09d791e4a559b42c6fc68fad3fa36e81443
Fabio Neto <fridow8@gmail.com>
2025-09-18 01:24:30 -0300

    asdasddzzzz

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -81,7 +81,7 @@ export default function LoginPage() {
               )}
 
               <div className="space-y-2">
-                <Label htmlFor="email">Emailasdasd</Label>
+                <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
                   type="email"
